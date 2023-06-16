79cf6214c2efa9984ab9997f4b3f1b5b81eb5fb6
Asenate Protti <136763852+asenatemprotti@users.noreply.github.com>
2023-06-16 09:29:30 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,47 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Cannot divide by zero."
+
+# Main calculator loop
+while True:
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter choice (1-5): ")
+
+    if choice == '5':
+        print("Calculator exited.")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result:", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid input. Please try again.")
